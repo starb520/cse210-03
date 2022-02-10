9d9c0ddd1e8b15564849be326f0aec2880eca1e9
starb520 <97459015+starb520@users.noreply.github.com>
2022-02-09 20:17:03 -0700

    deleted extra commented out code

--- Jumper_Game/Game.py
+++ Jumper_Game/Game.py
@@ -78,44 +78,13 @@ class Game:
         else:
             self._terminal.write_text("Thanks for playing.")
             self._is_playing = False
-             
-
-
-
-        
+               
 
 game = Game()
 game.play_game()
 
 
 
-
-# word_blanks = [False for i in range(len(rand_word._word))]
-# print(word_blanks)
-
-# print(rand_word._word)
-
-# letter_guess = " "
-# while not(letter_guess.isalpha()) or len(letter_guess) != 1:
-#     letter_guess = terminal.read_text("Guess a letter from [a-z]: ")
-#     if not(letter_guess.isalpha()):
-#         terminal.write_text("Choose a letter.")
-#     elif len(letter_guess)!= 1:
-#         terminal.write_text("Only one letter.")
-
-
-
-# letter_is_in_word = False
-# for letter in range(len(rand_word._word)):
-#     if rand_word._word[letter] == letter_guess:
-#         word_blanks[letter] = True
-
-# print(word_blanks)
-# for i in range(len(word_blanks)):
-#     if word_blanks[i] == True:
-#         print(rand_word._word[i], end=" ")
-#     else:
-        # print("_", end=" ")
 
 
 
